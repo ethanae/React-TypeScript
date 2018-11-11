65991f338b3a5141998c6440a1b93f713b31f989
Ethan Emandien <eemandien@mweb.co.za>
2018-11-11 11:26:10 +0200

    add db connection error handling

--- src/server/data/schemas/User.ts
+++ src/server/data/schemas/User.ts
@@ -2,6 +2,9 @@ import { Document, Schema, Model, model, connect } from "mongoose";
 import { IUser } from "../../common/interfaces/IUser";
 require('dotenv').config();
 
+let dbConnection;
+let collectionName;
+
 export interface UserEntity extends IUser, Document {}
 
 export const UserSchema: Schema = new Schema({
@@ -14,8 +17,13 @@ export const UserSchema: Schema = new Schema({
 });
 
 try {
-  connect(process.env.DB_CONN_STRING, { useNewUrlParser: true });
+  dbConnection = process.env.DB_CONN_STRING;
+  collectionName = process.env.COLL_NAME;
+  if(!dbConnection || !collectionName) {
+    throw new Error('Cannot connect to MongoDB. Ensure your connection string and collection name are correctly present in your .env file');
+  }
+  connect(dbConnection, { useNewUrlParser: true });
 } catch (error) {
-  console.log(error);
+  throw error;
 }
-export const User: Model<UserEntity> = model<UserEntity>('User', UserSchema, process.env.COLL_NAME);
+export const User: Model<UserEntity> = model<UserEntity>('User', UserSchema, collectionName);
